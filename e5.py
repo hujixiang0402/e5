43077a162fcfbc49c32bbeb7915c9f9d8bd4918e
hujixiang0402 <102725537+hujixiang0402@users.noreply.github.com>
2023-04-26 22:17:43 +0800

    Create e5.py

---
+++ e5.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print('Time is up!')
+
+t = 25 * 60  # 25 分钟的秒数
+countdown(t)
